b1c54c7a42e68a10ba05c6bf2eccf65dfe456744
Kevin Batista <k.luis360@gmail.com>
2022-11-16 15:40:59 -0300

    sytle: adicionando card que ficará a sala

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import "./App.css";
+import Sala from "./Sala/Sala";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <label className='App-label'>Enem 2022</label>
-        <label className='App-label' style={{fontSize: "32px"}}>Sala I7 - FGA</label>
-        <button className='App-header-button'>Restart</button>
+        <label className="App-label">Enem 2022</label>
+        <label className="App-label" style={{ fontSize: "32px" }}>
+          Sala I7 - FGA
+        </label>
+        <button className="App-header-button">Restart</button>
       </header>
+      <div className="App-body">
+        <Sala />
+      </div>
       
     </div>
   );
---
+++ src/components/Sala/Sala.js
@@ -0,0 +1,11 @@
+import "./Sala.css";
+
+const Sala = () => {
+  return (
+    <div className="Sala-container">
+      <div className="Sala-card"></div>
+    </div>
+  );
+};
+
+export default Sala;
