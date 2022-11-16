b5a2b66efed5a79288e86aa75ca9f15ea45da107
Kevin Batista <k.luis360@gmail.com>
2022-11-16 13:09:46 -0300

    style: adding header to main page

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <label className='App-label'>Enem 2022</label>
+        <label className='App-label' style={{fontSize: "32px"}}>Sala I7 - FGA</label>
+        <button className='App-header-button'>Restart</button>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
